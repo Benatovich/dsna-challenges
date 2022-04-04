732620ea36de26c471274144d599baffe35d0b68
Benatovich <sambenatovich@gmail.com>
2022-04-04 13:28:12 -0400

    challenge 1 completed by jordan

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+// challenge 1: reverse words
+
+function reverseWords(sentence) {
+  return sentence
+    .split(' ')
+    .map((word) => {
+      return word.split('').reverse().join('');
+    })
+    .join(' ');
+}
+
+console.log(reverseWords('here is a sentence'));
