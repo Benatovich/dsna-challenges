dfa52940b8c4603ede5a53a6bb1e3dfc598b3859
Benatovich <sambenatovich@gmail.com>
2022-04-04 13:57:31 -0400

    evenish/oddish complete

--- sandbox.js
+++ sandbox.js
@@ -10,3 +10,26 @@ function reverseWords(sentence) {
 }
 
 console.log(reverseWords('here is a sentence'));
+
+// challenge 2: title case
+
+// challenge 3: oddish or evenish 
+// expected input 121
+// expected output 'evenish' as a string because 1 + 2 + 1 = 4
+
+function oddishOrEvenish(number) {
+  const numb = number
+    .toString()
+    .split('')
+    .map((digit) => {
+      return parseInt(digit);
+    })
+    .reduce((oldDigit, newDigit) => {
+      return oldDigit + newDigit;
+    });
+    
+  return numb % 2 === 0 ? 'Evenish' : 'Oddish';
+
+}
+
+console.log(oddishOrEvenish(120));
