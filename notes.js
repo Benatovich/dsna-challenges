cb7901b77305b1060a2a135868e9c785c6e5a87a
Benatovich <sambenatovich@gmail.com>
2022-04-04 15:31:05 -0400

    anagrams complete

---
+++ notes.js
@@ -0,0 +1,21 @@
+// Write a function that takes a positive integer n, and returns an array that contains, in order, all of the numbers from 1 to n. But for multiples of three (evenly divisible by three) print “Fizz” instead of the number and for the multiples of five use “Buzz”. For numbers which are multiples of both three and five use “FizzBuzz”.
+// input
+// 16
+
+// Output
+// [
+//   1, 2, 'Fizz', 4, 'Buzz',
+//   'Fizz', 7, 8, 'Fizz', 'Buzz',
+//   11, 'Fizz', 13, 14, 'FizzBuzz',
+//   16
+// ]
+
+
+
+
+// Write a function that takes two words as arguments and returns true if they are anagrams (contain the exact same letters) and false otherwise.
+
+
+// Test Cases
+// 'superintended', 'unpredestined'	true
+// 'pictorialness', 'documentarily'	false
--- sandbox.js
+++ sandbox.js
@@ -32,4 +32,30 @@ function oddishOrEvenish(number) {
 
 }
 
-console.log(oddishOrEvenish(120));
+console.log(oddishOrEvenish(121));
+console.log(oddishOrEvenish(421));
+
+
+// function takes two words as arguments and returns true if they are anagrams
+// expected: 'superintended', 'unpredestined'	true
+// expecteD: 'pictorialness', 'documentarily'	false
+
+function anagrams(wordOne, wordTwo) {
+  // if they're anagrams, the words must be the same length
+  const lengthOne = wordOne.length;
+  const lengthTwo = wordTwo.length;
+  
+  if(lengthOne !== lengthTwo) {
+    return false;
+  } else {
+    //   I want to break each word down into an array of letters, sort those arrays, and see if they're equal
+    const stringOne = wordOne.split('').sort().join('');
+    const stringTwo = wordTwo.split('').sort().join('');
+        
+    return stringOne === stringTwo ? true : false;
+  } 
+}
+
+console.log(anagrams('mail', 'liam'));
+console.log(anagrams('mails', 'liam'));
+console.log(anagrams('tail', 'liam'));
